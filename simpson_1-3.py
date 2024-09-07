9c885f43ec06832ff485cc10dfe37c6d52617dd8
oscar-monsalve <oscarmonsalvec5@gmail.com>
2024-09-07 13:51:45 -0500

    update

---
+++ simpson_1-3.py
@@ -0,0 +1,10 @@
+def simpson13(a: float, b: float, n: int) -> float:
+    """
+    Returns the integral numberical value using the Simpson 1/3 method.
+
+    Args:
+    a: integral lower limit.
+    b: integral upper limit.
+    n: even number of divisions. The higher n is, the more precise is the integral result.
+    """
+    pass
